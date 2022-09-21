c21bb504b647a8eb049c90aabbd7387d37e9ea66
edumaldonado <edumaldonado@gmail.com>
2022-09-21 18:00:41 -0300

    fin del dia trabajo 2022-09-21

--- src/Components/AjaxApis.js
+++ src/Components/AjaxApis.js
@@ -11,12 +11,33 @@ return(
 
 export default class AjaxApis extends Component{
     state={
-        pockemons:[]
+        pokemons:[]
     }
 componentDidMount(){
     let url ="https://pokeapi.co/api/v2/pokemon";
     fetch(url)
     .then(res=> res.json())
+    .then((json) =>{
+        console.log(json);
+        json.results.forEach((el) =>{
+            console.log("--------------------------------");
+            //console.log(el);
+            fetch(el.url)
+            .then (res => res.json())
+            .then ((json)=>{
+                //console.log(json);
+                let pokemon ={
+                    id:json.id,
+                    name: json.name,
+                    avatar:json.sprites.front_default,
+                }
+                let pokemons = [...this.state.pokemons,pokemon];
+                this.setState({pokemons});
+                console.log(this.state.pokemons);
+            });
+        });
+        console.log(this.state.pokemons);
+    })
 
 }
 
@@ -24,6 +45,7 @@ render(){
     return(
         <>
         <h2>Peticiones Asincronas en componentes de clase</h2>
+        {this.state.pokemons.map(el => <Pokemon key={el.id} name = {el.name} avatar = {el.avatar}/>)}
         </>
     
     )
